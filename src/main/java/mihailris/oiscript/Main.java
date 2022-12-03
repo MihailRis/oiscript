f99e9c1a60bfb16aa76510fa4aa3b48d2a356056
MihailRis <mexaruc@gmail.com>
2022-12-04 02:16:15 +0300

    Main.java modified 3

--- src/main/java/mihailris/oiscript/Main.java
+++ src/main/java/mihailris/oiscript/Main.java
@@ -7,7 +7,6 @@ import mihailris.oiscript.stdlib.LibStd;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
