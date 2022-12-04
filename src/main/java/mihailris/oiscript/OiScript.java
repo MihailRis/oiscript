7da11ff56b3bd11d794353e638fabf52bf382ae9
MihailRis <mexaruc@gmail.com>
2022-12-04 14:41:23 +0300

    Version changed to 0.9.0

--- src/main/java/mihailris/oiscript/OiScript.java
+++ src/main/java/mihailris/oiscript/OiScript.java
@@ -11,8 +11,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class OiScript {
-    public static final int VERSION_MAJOR = 1;
-    public static final int VERSION_MINOR = 0;
+    public static final int VERSION_MAJOR = 0;
+    public static final int VERSION_MINOR = 9;
     public static final int VERSION_PATCH = 0;
     public static final String VERSION_STRING = VERSION_MAJOR+"."+VERSION_MINOR+"."+VERSION_PATCH;
 
